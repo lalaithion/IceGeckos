17c9bfe170f406d920de8bf58d402b2d729a1c20
Alexander Curtiss <apccurtiss@gmail.com>
2017-04-15 14:53:37 -0600

    Appeased Izaak

--- meraki/update.py
+++ meraki/update.py
@@ -9,33 +9,33 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-client_list = []
-
-org_url = 'https://dashboard.meraki.com/api/v0/organizations'
-orgs = requests.get(org_url, headers=headers).json()
-
-for org in orgs:
-    net_url = 'https://dashboard.meraki.com/api/v0/organizations/' + str(org['id']) + '/networks'
-    networks = requests.get(net_url, headers=headers).json()
-
-    for network in networks:
-        device_url = 'https://dashboard.meraki.com/api/v0/networks/' + network['id'] + '/devices'
-        devices = requests.get(device_url, headers=headers).json()
-
-        for device in devices:
-            client_url = 'https://dashboard.meraki.com/api/v0/devices/' + device['serial'] + '/clients?timespan=' + str(timespan)
-            clients = requests.get(client_url, headers=headers).json()
-
-            for client in clients:
-                client_list.append({
-                    'info': client,
-                    'org': org,
-                    'network': network,
-                    'device': device,
-                })
-
 def update():
     while True:
+        client_list = []
+
+        org_url = 'https://dashboard.meraki.com/api/v0/organizations'
+        orgs = requests.get(org_url, headers=headers).json()
+
+        for org in orgs:
+            net_url = 'https://dashboard.meraki.com/api/v0/organizations/' + str(org['id']) + '/networks'
+            networks = requests.get(net_url, headers=headers).json()
+
+            for network in networks:
+                device_url = 'https://dashboard.meraki.com/api/v0/networks/' + network['id'] + '/devices'
+                devices = requests.get(device_url, headers=headers).json()
+
+                for device in devices:
+                    client_url = 'https://dashboard.meraki.com/api/v0/devices/' + device['serial'] + '/clients?timespan=' + str(timespan)
+                    clients = requests.get(client_url, headers=headers).json()
+
+                    for client in clients:
+                        client_list.append({
+                            'info': client,
+                            'org': org,
+                            'network': network,
+                            'device': device,
+                        })
+                        
         with open('clients.json', 'w') as outfile:
             outfile.write(json.dumps(client_list, indent=2))
         time.sleep(60)
