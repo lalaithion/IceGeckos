fa2f93a54ca13ca7c3c96fd1233c80120602eb5d
Izaak Weiss <izwe5504@colorado.edu>
2017-04-15 12:05:36 -0600

    Created Meraki folder and added templates

--- website/home.py
+++ website/home.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from website import website
 
 @website.route('/')
 @website.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('home.html', variable='THING')
